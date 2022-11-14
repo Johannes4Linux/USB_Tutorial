7af294e0bf8db783cc03fbb8217c0ee82ea0b9c7
Johannes 4GNU/Linux <johannes@gnu-linux.rocks>
2022-11-14 15:16:31 +0100

    Added Python example for accessing endpoints

---
+++ 02_userspace_apps/Python/bulk_out.py
@@ -0,0 +1,27 @@
+import usb.core
+import sys
+
+dev = usb.core.find(idVendor = 0x03eb, idProduct=1)
+
+if dev == None:
+    print("No USB device found!")
+    sys,exit(0)
+
+# Get Configuration
+cfg = dev.get_active_configuration()
+
+# Get Interface
+intf = cfg[(0,0)] # (index, alternate settings index)
+
+# Get Endpoint
+ep = intf[0]
+
+# Set up data
+data = bytearray(8)
+for i in range(4):
+    data[i] = 0x3
+
+# Write to BULK OUT EP
+ep.write(data)
+
+
